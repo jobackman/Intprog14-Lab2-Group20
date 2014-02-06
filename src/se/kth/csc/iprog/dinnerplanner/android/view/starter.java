32eb89256c61dca3640ca42d512eb635b120a4e0
Johan Bäckman <johba@kth.se>
2014-02-06 16:30:36 +0100

    fucking starterlayout, die.

--- src/se/kth/csc/iprog/dinnerplanner/android/view/starter.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/starter.java
@@ -1,7 +1,14 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Set;
+import se.kth.csc.iprog.dinnerplanner.android.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
+
 /**
  * Created by Johan on 2014-02-06.
  */
@@ -14,8 +21,18 @@ public class starter {
     public starter(View view){
 
         this.view = view;
+        LinearLayout starterList = (LinearLayout) view.findViewById(R.id.starterLayout);
 
+        Set<Dish> starters = dynamicStarters.getDishesOfType(1);
+        for(Dish s : starters){
+            s.getImage();
+            
+        }
 
+       /**
+        participantsView = (TextView) view.findViewById(R.id.participantsText);
+        participantsView.setText("Participants: ");
+        **/
 
     }
 }
